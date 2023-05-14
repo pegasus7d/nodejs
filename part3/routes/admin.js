9f8f52ab0f49de4a3ab738cc181156323e65c90a
pegasus7d <debayanbiswas1111@gmail.com>
2023-05-15 04:37:54 +0530

    added the ejs template

--- part3/routes/admin.js
+++ part3/routes/admin.js
@@ -6,19 +6,21 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const products=[];
+const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product',{pageTitle:'Add Product',path:'/admin/add-product'})
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle:'Add Product', path: '/admin/add-product', activeAddProduct: true, formCSS: true, productCSS: true});
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
- products.push({title:req.body.title})
+  products.push({
+    'title': req.body.title
+  });
   res.redirect('/');
 });
 
-exports.routes=router
-exports.products=products
-
+module.exports.router = router;
+module.exports.products = products;
